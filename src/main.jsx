52789091b88a2f75a848b31fb28c014df34d4585
Escannors <39978304+nhp29@users.noreply.github.com>
2026-08-18 11:37:25 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './app.jsx'
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <React.StrictMode>
-      <App />
+      <app />
     </React.StrictMode>,
   )
 }
